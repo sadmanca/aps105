e7d6e7d1a6eb987567ee07d36740949421a0edf6
Sadman Hossain <m.sadman.h@gmail.com>
2022-02-07 17:13:15 +0000

    Add tutorial 2 questions 1, 2

---
+++ tut2/tut2_1.c
@@ -0,0 +1,10 @@
+/*
+TUTORIAL 2
+
+Question 1:
+
+Write a single statement in C that declares a boolean variable named divisible
+and assigns true to divisble if and only if the value stored in an int variable
+position is exactly divisible by 5 or 7. Assume position variable is already
+declared and initialized.
+*/
---
+++ tut2/tut2_2.c
@@ -0,0 +1,22 @@
+/*
+TUTORIAL 2
+
+Question 2:
+
+Akarsh is in the wild, and he wants to cross a path. He observes that this path
+is divided into small blocks, where every three blocks he observes small needles
+and every four blocks he observes rocks. He does not want to step on rocks or
+needles. Akarsh is strong enough to jump from 8 to 20 blocks in one jump. 
+
+Write a program in C that helps Akarsh jump a random number of blocks from 8 to
+20 and tells him if he is stepping on rocks or needles or both. Assume Akarsh is
+standing on block 0, where there are rocks or needles, but Akrash is standing on
+a wooden piece.
+
+Sample output is: 
+
+    He will jump 17 blocks and hit the ground! Yaay! 
+    He will jump 15 blocks and hit needles.
+    He will jump 9 blocks and hit needles and rocks.
+
+*/
