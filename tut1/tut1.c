b30f99883ec8b02c03442550054d55885896f39b
Sadman Hossain <m.sadman.h@gmail.com>
2022-02-07 17:13:05 +0000

    Add tutorial 1 question

---
+++ tut1/tut1.c
@@ -0,0 +1,24 @@
+/*
+TUTORIAL 1
+
+Question:
+
+Write a C program that asks the user to enter a time instant in the month, by
+sequentially entering the day in the month, the hour in the day, the minute in
+the hour, and the second in the minute. Calculate (1) how many seconds have
+elapsed since the first second of the month; (2) how many minutes have elapsed
+since the first minute; (3) how many hours have elapsed since the first hour;
+and (4) how many days have elapsed since the first day (day 1). All
+floating-point values to be printed should be rounded to two decimal places.
+
+A run of your program should look like the following:
+
+    Enter the day in the month: 15
+    Enter the hour in the day: 20
+    Enter the minute in the hour: 15
+    Enter the second in the minute: 30
+
+    Do you know that time is precious?
+    1282530 seconds, or 21375.50 minutes, or 356.26 hours, or 14.84 days have elapsed since the beginning of the month
+    
+*/
