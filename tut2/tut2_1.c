4ba26ff7b435fc2726691956c0bf463f5d7ddec2
Sadman Hossain <m.sadman.h@gmail.com>
2022-02-07 17:14:49 +0000

    Add general solution for tutorial 2 question 1

--- tut2/tut2_1.c
+++ tut2/tut2_1.c
@@ -7,4 +7,13 @@ Write a single statement in C that declares a boolean variable named divisible
 and assigns true to divisble if and only if the value stored in an int variable
 position is exactly divisible by 5 or 7. Assume position variable is already
 declared and initialized.
+
+SOLUTION:
+
+    bool divisible = position % 7 == 0 || position % 5 == 0;
+
+OR using De Morgan's Law can be written as:
+
+    bool divisible = !(position % 7 != 0 && position % 5 != 0);
+    
 */
