9c5db9888c0cfd2f84013273344a3c30b4db5c0c
Sadman Hossain <m.sadman.h@gmail.com>
2022-02-07 17:15:11 +0000

    Add tutorial 3 question

---
+++ tut3/tut3.c
@@ -0,0 +1,24 @@
+/*
+TUTORIAL 3
+
+Question:
+
+Write a complete C program that first prompts the user to enter a floating point
+number between 0 and 10, and its number of digits after the decimal point (no
+greater than 7). It will then print all possible two-digit numbers found in
+consecutive digits of the floating point number that the user entered. For
+example, if the number entered is 2.7182818, and the number of digits entered is
+7, the two-digit numbers printed are:
+
+    Enter a floating point number between 0 and 10: 2.7182818
+    Enter the number of digits after the decimal point: 7
+    27
+    71
+    18
+    82
+    28
+    81
+    18
+
+*/
+
