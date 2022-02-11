99404561d6c9d979fc849bf0a6481c8c1e706f50
Sadman Hossain <m.sadman.h@gmail.com>
2022-02-11 22:18:28 +0000

    Add solution for lab 5 part 1

--- lab5/lab5_1.c
+++ lab5/lab5_1.c
@@ -12,36 +12,57 @@ For example, if I have int a[] = {1, 2, 10, 12, 100}; and int b[] = {3, 11,
 */
 
 #include <stdio.h>
-#include <math.h>
-#include <stdbool.h>
-
-// double array_median(int a[], int sizeA) {
-//     if (sizeA % 2 == 0) {
-//         return ((double) a[sizeA / 2] + a[(sizeA / 2) + 1]) / 2;
-//     } else {
-//         return (double) a[sizeA / 2];
-//     }
-// }
 
 double median(int a[], int b[], int sizeA, int sizeB) {
-    // return (array_median(a, sizeA) + array_median(b, sizeB)) / 2;
-    int combined[sizeA + sizeB];
-    for (int i = 0; i < sizeA; i++) {
-        combined[i] = a[i];
-    }
-    bool is_combined = false;
-    for (int i = 0; i < sizeB; i++) {
-        while (!is_combined) {
-            
+    int combined_size = sizeA + sizeB;
+    int combined[combined_size];
 
-            if (sizeof(combined)/sizeof(combined[0]) == sizeA + sizeB) {
-                is_combined = true;
-            }
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < sizeA && j < sizeB) {
+        if (a[i] < b[j]) {
+            combined[k] = a[i];
+            i++;
+        } else {
+            combined[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+
+    if (i == sizeA) {
+        while (j < sizeB) {
+            combined[k] = b[j];
+            j++;
+            k++;
         }
     }
-    
+
+    if (j == sizeB) {
+        while (i < sizeA) {
+            combined[k] = a[i];
+            i++;
+            k++;
+        }
+    }
+
+    if (combined_size % 2 == 0) {
+        return ((double) combined[combined_size / 2] + combined[combined_size / 2 - 1]) / 2;
+    } else {
+        return (double) combined[combined_size / 2];
+    }
 }
 
 int main(void) {
+    // test median method
+    // int a[] = {1, 2, 10, 12, 100};
+    // int b[] = {3, 11, 20, 500, 600};
+    int a[] = {1, 2, 10};
+    int b[] = {500, 600};
+    int sizea = sizeof(a)/sizeof(a[0]);
+    int sizeb = sizeof(b)/sizeof(b[0]);
+    printf("%.1lf", median(a, b, sizea, sizeb));
     return 0;
 }
