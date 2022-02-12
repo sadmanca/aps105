004baeb63328f2b8676089bc7bad98733df0497a
Sadman Hossain <m.sadman.h@gmail.com>
2022-02-12 19:26:56 +0000

    Add tutorial 4 question

---
+++ tut4/tut4.c
@@ -0,0 +1,34 @@
+/*
+
+TUTORIAL 4
+Question:
+
+Write a C program that receives an input number and checks if it can be
+expressed as the sum of two prime numbers. If it is, print out all possible
+combinations.
+
+Your code should have a function named checkPrime, and it returns a boolean. The
+checkPrime function checks if the number is prime number and return a boolean
+value. Start by implementing this function first. Using break is okay, but not
+preferred coding style. You should know how to write your code without it.
+
+Sample output:
+
+    Enter a positive integer: 56
+    56 can be expressed as the sum of 3 and 53.
+    56 can be expressed as the sum of 13 and 43.
+    56 can be expressed as the sum of 19 and 37.
+
+Another sample output:
+
+    Enter a positive integer: 11
+    11 cannot be expressed as a sum of two prime numbers.
+
+NOT sample output:
+
+    The second input is redundant in the following example:
+    Enter a positive integer: 5
+    5 can be expressed as the sum of 2 and 3.
+    5 can be expressed as the sum of 3 and 2.
+
+*/
