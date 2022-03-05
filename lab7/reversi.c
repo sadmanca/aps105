fbefb12ec709f1d4d92c70d935ca9eb3d4aabfa5
Sadman Hossain <m.sadman.h@gmail.com>
2022-03-05 22:12:48 +0000

    Add lab 7 pdf handout and files

---
+++ lab7/reversi.c
@@ -0,0 +1,19 @@
+//
+// Author:
+//
+
+#include "reversi.h"
+
+void printBoard(char board[][26], int n) {
+}
+
+bool positionInBounds(int n, int row, int col) {
+}
+
+bool checkLegalInDirection(char board[][26], int n, int row, int col,
+                           char colour, int deltaRow, int deltaCol) {
+}
+
+int main(void) {
+  return 0;
+}
