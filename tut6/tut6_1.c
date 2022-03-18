43c4c78f1ad1dd2071ff57ae7752498d634036ec
Sadman Hossain <m.sadman.h@gmail.com>
2022-03-18 14:51:41 -0400

    Add tutorial 6 questions 1-2

---
+++ tut6/tut6_1.c
@@ -0,0 +1,27 @@
+/*
+TUTORIAL 6 Question 1:
+
+In this question, we will be implementing a string subtraction algorithm. The program will prompt the user to enter a random number of characters between 6 and 10 followed by another random number of characters between 3 and 5. The program will then remove the second string from the first string if it exists within it. Example runs of the program are shown below:
+
+    Enter 9 characters: abcdefghi
+    Enter 4 characters: defg
+
+    Phrase: abcdefghi
+    Remove: defg
+    Result: abchi
+
+    Enter 6 characters: abcdef
+    Enter 5 characters: tests
+
+    Phrase: abcdef
+    Remove: tests
+    Result: abcdef
+
+    Enter 8 characters: abcdefgh
+    Enter 4 characters: abcd
+
+    Phrase: abcdefgh
+    Remove: abcd
+    Result: efgh
+
+*/
---
+++ tut6/tut6_2.c
@@ -0,0 +1,19 @@
+/*
+TUTORIAL 6 Question 2:
+
+In this question, we will be implementing a string search system. Assume your program has the string "I go to the University of Toronto." already defined. Your task is to read a character input from the user and find the location(s) of that character in the string. Example outputs from the program are as follows:
+
+    String: "I go to the University of Toronto."
+    Enter a character: n
+    Found at index 13
+    Found at index 30
+
+    String: "I go to the University of Toronto."
+    Enter a character: a
+    Character not found.
+
+    String: "I go to the University of Toronto."
+    Enter a character: v
+    Found at index 15
+
+*/
