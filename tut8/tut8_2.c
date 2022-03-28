b47384f28a4d612f75d81af126b4f458669e8b7b
Sadman Hossain <m.sadman.h@gmail.com>
2022-03-28 11:10:19 -0400

    Add tutorial 8 question 2

---
+++ tut8/tut8_2.c
@@ -0,0 +1,33 @@
+/*
+TUTORIAL 8 Question 2:
+
+Given a chessboard, print all sequences of moves of a knight on a chessboard such that the knight visits every square only once. For example, for the standard 8 × 8 chessboards, below is one such tour. We have started the tour from the top-leftmost of the board (marked as 1), and the next number represents the knight’s consecutive moves.
+
+Sample output:
+
+   [1, 6, 15, 10, 21]
+   [14, 9, 20, 5, 16]
+   [19, 2, 7, 22, 11]
+   [8, 13, 24, 17, 4]
+   [25, 18, 3, 12, 23]
+
+   [1, 6, 11, 18, 21]
+   [12, 17, 20, 5, 10]
+   [7, 2, 15, 22, 19]
+   [16, 13, 24, 9, 4]
+   [25, 8, 3, 14, 23]
+
+   [1, 6, 11, 16, 21]
+   [12, 15, 20, 5, 10]
+   [7, 2, 13, 22, 17]
+   [14, 19, 24, 9, 4]
+   [25, 8, 3, 18, 23]
+
+   [1, 6, 17, 12, 21]
+   [16, 11, 20, 5, 18]
+   [7, 2, 9, 22, 13]
+   [10, 15, 24, 19, 4]
+   [25, 8, 3, 14, 23]
+   <<<... and 300 other knight’s tours>>>
+
+*/
